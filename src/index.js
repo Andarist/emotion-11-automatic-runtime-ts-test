801a5941ba685459ed0809756d2ad665ef907e29
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-11-10 15:38:10 +0100

    patch @emotion/react as well

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 "use strict";
 exports.__esModule = true;
 var jsx_runtime_1 = require("@emotion/react/jsx-runtime");
-exports["default"] = jsx_runtime_1.jsx("div", { css: "fas" }, void 0);
+exports["default"] = jsx_runtime_1.jsx("div", { css: document.createElement('div') }, void 0);
